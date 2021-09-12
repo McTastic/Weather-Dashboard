a60193d4c69084bd07e9dcea938d9d1be1aa303c
McTastic <mctastic2121@gmail.com>
2021-09-12 14:54:17 -0400

    added fetch for weather API to gather weather data for current day on entered city name

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+var APIKey = "ae17a1c3ab60e66fadd0d139b5b7cf56";
+var city;
+var cityName = $(".cityName");
+var cityTemp = $(".cityTemp");
+var cityWind = $(".cityWind");
+var cityIcon = $(".cityIcon");
+var cityDescription = $(".cityDescription");
+var userInput = $(".form-control");
+submitBtn = $(".btn");
+
+submitBtn.on("click", function(){
+    event.preventDefault();
+    city = userInput.val().trim(); 
+    if(city){
+    getCityWeather(city);
+    cityName.text("");
+    userInput.val("");
+    cityIcon.empty("img");
+    }else{
+        alert("Please enter a city")
+    }
+})
+var getCityWeather = function(){
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+fetch(queryURL).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        var cityNameVal = data.name;
+        var cityTempVal = data.main.temp;
+        var cityWindVal = data.wind.speed;
+        var cityIconVal = $("<img />", {src: "http://openweathermap.org/img/wn/"+ data.weather[0].icon + "@4x.png"});
+        var cityDescriptionVal = data.weather[0].description;
+          cityName.text(cityNameVal);
+          cityTemp.text("Temp: " + cityTempVal +"℉");
+          cityWind.text("Wind: " + cityWindVal +" M/hr");
+          cityIcon.append(cityIconVal); 
+          cityDescription.text(cityDescriptionVal);
+        console.log(data);
+      });
+    } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function (error) {
+        alert('Unable to connect to OpenWeather');
+      });
+  };
+
+//   var getCityIcon = function(){
+//     var queryURL = "http://openweathermap.org/img/wn/"+ cityIconCode + "@2x.png";
+//     fetch(queryURL).then(function (response) {
+//         if (response.ok) {
+//           response.json().then(function (data) {
+//              var cityIconVal = data["weather"][0]["icon"];
+
+//   }
+
+// var cityIconVal = "http://openweathermap.org/img/wn/"+data.weather[0].icon + ".png";
