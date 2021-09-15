672c2f8a3c6f737335915370fdfad57ff19c53cb
McTastic <mctastic2121@gmail.com>
2021-09-14 20:12:43 -0400

    added search history buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@ var APIKey = "ae17a1c3ab60e66fadd0d139b5b7cf56";
 var city;
 var lat;
 var lon;
+var cityList = $(".cityList");
 var cityName = $(".cityName");
 var cityTemp = $(".cityTemp");
 var cityWind = $(".cityWind");
 var cityIcon = $(".cityIcon");
 var cityDescription = $(".cityDescription");
 var cityUVIndex = $(".cityUVIndex");
+var UVIText = $(".UVIText");
 var userInput = $(".form-control");
 var forecastHeader = $(".forecastHeader");
 var forecastCard = $(".forecastCard");
@@ -26,14 +28,32 @@ submitBtn.on("click", function () {
     forecastHeader.text("5-Day Forecast");
     getCityWeather();
     getForecast();
+    saveCity();
     cityName.text("");
     userInput.val("");
     cityIcon.empty("img");
     forecastCard.empty();
+    UVIText.empty();
   } else {
     alert("Please enter a city");
   }
 });
+
+$("body").on("click","#cityBtn", function(){ 
+    event.preventDefault();
+    city = this.textContent;
+    getCityWeather();
+    getForecast();
+    cityIcon.empty("img");
+    forecastCard.empty();
+    UVIText.empty();
+});
+
+function saveCity(){
+    localStorage.setItem("Saved City", city);
+    var getCity = localStorage.getItem("Saved City");
+    cityList.append("<li><button id='cityBtn'>"+getCity+"</bu</li>");
+}
 
 function init() {
   forecastHeader.css("display", "none");
@@ -107,78 +127,117 @@ var getForecast = function () {
           "&units=imperial"
       ).then(function (response) {
         if (response.ok) {
-          console.log(response);
           response.json().then(function (data) {
             var forecastArray = data.daily;
-            console.log(forecastArray);
+            UVIText.append("<h4>UVI: </h4>");
             cityUVIndex.text(data.current.uvi);
              if(parseInt(cityUVIndex.text()) <= 2){
             cityUVIndex.css("background-color","green")
-             } else if(parseInt(cityUVIndex.text()) > 2 && parseInt(cityUVIndex.text())<= 6){
+             }  if(parseInt(cityUVIndex.text()) > 2 && parseInt(cityUVIndex.text()) <= 6){
                  cityUVIndex.css("background-color","yellow")
-             } else {
+             } if(parseInt(cityUVIndex.text()) > 6) {
                  cityUVIndex.css("background-color","red")
              };
             for (var i = 0; i < forecastArray.length - 3; i++) {
               if (i === 0) {
+                var cityIconVal = $("<img />", {
+                    src:
+                      "http://openweathermap.org/img/wn/" +
+                      data.daily[0].weather[0].icon +
+                      "@2x.png",
+                  });
                 Day1.append(
                   "<div class ='maxTemp1'></div>",
                   "<div class ='minTemp1'></div>",
                   "<div class ='wind1'></div>",
-                  "<div class ='humidity1'></div>"
+                  "<div class ='humidity1'></div>",
+                  "<div class = 'icon1'></div>" 
                 );
-                $(".maxTemp1").text(forecastArray[0].temp.max + "℉");
-                $(".minTemp1").text(forecastArray[0].temp.min + "℉");
-                $(".wind1").text(forecastArray[0].wind_speed);
-                $(".humidity1").text(forecastArray[0].humidity + " %");
+                $(".maxTemp1").text("High: " + forecastArray[0].temp.max + "℉");
+                $(".minTemp1").text("Low: " + forecastArray[0].temp.min + "℉");
+                $(".wind1").text("Wind: " + forecastArray[0].wind_speed + "M/hr");
+                $(".humidity1").text("Humidity: " + forecastArray[0].humidity + " %");
+                $(".icon1").append(cityIconVal);
               }
               if (i === 1) {
+                var cityIconVal = $("<img />", {
+                    src:
+                      "http://openweathermap.org/img/wn/" +
+                      data.daily[1].weather[0].icon +
+                      "@2x.png",
+                  });
                 Day2.append(
                   "<div class ='maxTemp2'></div>",
                   "<div class ='minTemp2'></div>",
                   "<div class ='wind2'></div>",
-                  "<div class ='humidity2'></div>"
+                  "<div class ='humidity2'></div>",
+                  "<div class = 'icon2'></div>" 
                 );
-                $(".maxTemp2").text(forecastArray[1].temp.max + "℉");
-                $(".minTemp2").text(forecastArray[1].temp.min + "℉");
-                $(".wind2").text(forecastArray[1].wind_speed);
-                $(".humidity2").text(forecastArray[1].humidity + " %");
+                $(".maxTemp2").text("High: " + forecastArray[1].temp.max + "℉");
+                $(".minTemp2").text("Low: " +forecastArray[1].temp.min + "℉");
+                $(".wind2").text("Wind: " + forecastArray[1].wind_speed + "M/hr");
+                $(".humidity2").text("Humidity: " + forecastArray[1].humidity + " %");
+                $(".icon2").append(cityIconVal);
               }
               if (i === 2) {
+                var cityIconVal = $("<img />", {
+                    src:
+                      "http://openweathermap.org/img/wn/" +
+                      data.daily[2].weather[0].icon +
+                      "@2x.png",
+                  });
                 Day3.append(
                   "<div class ='maxTemp3'></div>",
                   "<div class ='minTemp3'></div>",
                   "<div class ='wind3'></div>",
-                  "<div class ='humidity3'></div>"
+                  "<div class ='humidity3'></div>",
+                  "<div class = 'icon3'></div>" 
                 );
-                $(".maxTemp3").text(forecastArray[2].temp.max + "℉");
-                $(".minTemp3").text(forecastArray[2].temp.min + "℉");
-                $(".wind3").text(forecastArray[2].wind_speed);
-                $(".humidity3").text(forecastArray[2].humidity + " %");
+                $(".maxTemp3").text("High: " + forecastArray[2].temp.max + "℉");
+                $(".minTemp3").text("Low: " + forecastArray[2].temp.min + "℉");
+                $(".wind3").text("Wind: " + forecastArray[2].wind_speed + "M/hr");
+                $(".humidity3").text("Humidity: " + forecastArray[2].humidity + " %");
+                $(".icon3").append(cityIconVal);
               }
               if (i === 3) {
+                var cityIconVal = $("<img />", {
+                    src:
+                      "http://openweathermap.org/img/wn/" +
+                      data.daily[3].weather[0].icon +
+                      "@2x.png",
+                  });
                 Day4.append(
                   "<div class ='maxTemp4'></div>",
                   "<div class ='minTemp4'></div>",
                   "<div class ='wind4'></div>",
-                  "<div class ='humidity4'></div>"
+                  "<div class ='humidity4'></div>",
+                  "<div class = 'icon4'></div>" 
                 );
-                $(".maxTemp4").text(forecastArray[3].temp.max + "℉");
-                $(".minTemp4").text(forecastArray[3].temp.min + "℉");
-                $(".wind4").text(forecastArray[3].wind_speed);
-                $(".humidity4").text(forecastArray[3].humidity + " %");
+                $(".maxTemp4").text("High: " + forecastArray[3].temp.max + "℉");
+                $(".minTemp4").text("Low: " + forecastArray[3].temp.min + "℉");
+                $(".wind4").text("Wind: " + forecastArray[3].wind_speed + "M/hr");
+                $(".humidity4").text("Humidity: " + forecastArray[3].humidity + " %");
+                $(".icon4").append(cityIconVal);
               }
               if (i === 4) {
+                var cityIconVal = $("<img />", {
+                    src:
+                      "http://openweathermap.org/img/wn/" +
+                      data.daily[4].weather[0].icon +
+                      "@2x.png",
+                  });
                 Day5.append(
                   "<div class ='maxTemp5'></div>",
                   "<div class ='minTemp5'></div>",
                   "<div class ='wind5'></div>",
-                  "<div class ='humidity5'></div>"
+                  "<div class ='humidity5'></div>",
+                  "<div class = 'icon5'></div>" 
                 );
-                $(".maxTemp5").text(forecastArray[4].temp.max + "℉");
-                $(".minTemp5").text(forecastArray[4].temp.min + "℉");
-                $(".wind5").text(forecastArray[4].wind_speed);
-                $(".humidity5").text(forecastArray[4].humidity + " %");
+                $(".maxTemp5").text("High: " + forecastArray[4].temp.max + "℉");
+                $(".minTemp5").text("Low: " + forecastArray[4].temp.min + "℉");
+                $(".wind5").text("Wind: " + forecastArray[4].wind_speed + "M/hr");
+                $(".humidity5").text("Humidity: " + forecastArray[4].humidity + " %");
+                $(".icon5").append(cityIconVal);
               }
             }
           });
@@ -186,7 +245,4 @@ var getForecast = function () {
       });
     });
 };
-
-
-
 init();
